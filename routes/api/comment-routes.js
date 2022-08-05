16b43363cc51b50ddf4e7db767a688b789770731
abbygraves <abbygraves14@gmail.com>
2022-08-05 18:51:28 -0500

    comment capablitities

---
+++ routes/api/comment-routes.js
@@ -0,0 +1,51 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+
+
+// GET ALL COMMENTS
+router.get('/', (req, res) => {
+  Comment.findAll()
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+
+// CREATE A COMMENT
+router.post('/', (req, res) => {
+  Comment.create({
+    comment_text: req.body.comment_text,
+    user_id: req.body.user_id,
+    post_id: req.body.post_id
+  })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+
+// DELETE - /API/COMMENTS/1
+router.delete('/:id', (req, res) => {
+  Comment.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
